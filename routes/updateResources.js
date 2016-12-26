26d60aa4a99facc0a0eff31db6811b28ceaf0e67
wiljago <wil.cook.is@gmail.com>
2016-12-26 17:52:27 -0500

    banana banana bananan

--- routes/updateResources.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('updateResources', { title: 'Syria Sources' });
-});
-
-module.exports = router;
